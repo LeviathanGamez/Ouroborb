8efbeddf4dec37cabd51737c8947a3fa31d89843
Miguel Franjul <144841890+LeviathanGamez@users.noreply.github.com>
2026-07-06 14:22:36 -0400

    Added click SFX
    Added click SFX for mouse clicking on tiles with critical hit SFX variation.

--- new-game-project/scripts/tile.gd
+++ new-game-project/scripts/tile.gd
@@ -4,6 +4,8 @@ extends Node2D
 @onready var animation_player = $HitFlashAnimationPlayer
 @onready var label = $Label
 @onready var sprite = $Sprite2D
+@onready var audio_player = $AudioStreamPlayer2D
+@onready var audio_player2 = $AudioStreamPlayer2D2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_to_group("Colliders")
@@ -23,9 +25,13 @@ func _on_button_pressed() -> void:
 		GlobalGameManager.add_count(GlobalGameManager.player_value*GlobalGameManager.player_crit_mult)
 		value -= GlobalGameManager.player_strength * GlobalGameManager.player_crit_mult
 		sprite.material.set_shader_parameter("Tint", Color.html("EFBF04"))
+		audio_player2.pitch_scale= randf_range(0.9,1.25)
+		audio_player2.play()
 		#hitflash
 	else:
 		sprite.material.set_shader_parameter("Tint", Color.WHITE)
 		value -= GlobalGameManager.player_strength
 		GlobalGameManager.add_count(GlobalGameManager.player_value)
+		audio_player.pitch_scale= randf_range(0.9,1.25)
+		audio_player.play()
 	animation_player.play("hit_flash")
